bf39cb9724b661727f4bbf73dc4c4d7968dc2434
LostzSoul <61343588+LostzSoul@users.noreply.github.com>
2020-04-03 22:01:14 +0700

    OK มั้ง!!

---
+++ Assets/Code/ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+   
+    public GameObject goalGameObject, cubeTransform;
+   
+
+    public void Start()
+    {
+        cubeTransform.SetActive(true);    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(goalGameObject))
+        {
+            cubeTransform.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Code/held.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class held : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDrag()
+    {
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = transform.position.z - Camera.main.transform.position.z;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        transform.position = new Vector3(mouse.x, mouse.y, mouse.z);
+    }
+
+   
+}
